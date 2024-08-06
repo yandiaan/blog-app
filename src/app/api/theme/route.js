ede7e901eda66f734e632944819b549ead2e2840
Dian Setiawan <39780760+yandiaan@users.noreply.github.com>
2024-08-06 09:24:40 +0700

    anjayani

--- src/app/api/theme/route.js
+++ src/app/api/theme/route.js
@@ -6,8 +6,12 @@ export async function POST(req) {
 
   const secretKey = "CuDhieuyx6z0cVoo6QhB1w19Twasow81RHVOrgseCUfrOY7GSteTiDKWRMR2BvEgdVSLaiUlZIgb9fX58yGLhZ2c9bBzEBBXmbZvvmrVPiFILdmBAXUXXU4RyHxKUha3pG8T60B1qwOTZprsDITe3IqKZi1aRFIOp7dVSauZtxyFhoKzRXPM87yiOPY4cmGG1qxH8zH7lSAlA2FDftklGVTDyjLkdrC0x5MEbCCkxql2ge"
 
-  if(theme !== secretKey) NextResponse.json({ code: 503, message: "Service Unavailable!" });
+  if(!theme || theme !== secretKey) {
+    return NextResponse.json({ code: 503, message: "Service Unavailable!" });
+  }else{
+    return NextResponse.json({ code: 200, message: "Theme Fetch Succesfully" });
+  }
 
-  return NextResponse.json({ code: 200, message: "Theme Fetch Succesfully" });
+  
 }
 
