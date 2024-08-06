8ccf171e7793938338d71fab3fd01179b2f04b3c
Dian Setiawan <39780760+yandiaan@users.noreply.github.com>
2024-08-06 09:21:33 +0700

    theme api

--- src/app/api/theme/route.js
+++ src/app/api/theme/route.js
@@ -1,5 +1,13 @@
 import { NextResponse } from 'next/server';
 
-export async function GET() {
-  return NextResponse.json({ data: 'dsadasdadasdasfjasfasdkadasd' })
+export async function POST(req) {
+
+  const { theme } = req.json();
+
+  const secretKey = "CuDhieuyx6z0cVoo6QhB1w19Twasow81RHVOrgseCUfrOY7GSteTiDKWRMR2BvEgdVSLaiUlZIgb9fX58yGLhZ2c9bBzEBBXmbZvvmrVPiFILdmBAXUXXU4RyHxKUha3pG8T60B1qwOTZprsDITe3IqKZi1aRFIOp7dVSauZtxyFhoKzRXPM87yiOPY4cmGG1qxH8zH7lSAlA2FDftklGVTDyjLkdrC0x5MEbCCkxql2ge"
+
+  if(theme !== secretKey) NextResponse.json({ code: 503, message: "Service Unavailable!" });
+
+  return NextResponse.json({ code: 200, message: "Theme Fetch Succesfully" });
 }
+
